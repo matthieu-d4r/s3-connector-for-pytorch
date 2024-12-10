ad377f5b8b27fd9aab3940660fb116b782cd6a07
Matthieu Dufour <mdufou@amazon.co.uk>
2024-12-10 16:46:00 +0000

    temp

--- s3torchbenchmarking/src/s3torchbenchmarking/constants.py
+++ s3torchbenchmarking/src/s3torchbenchmarking/constants.py
@@ -1,7 +1,7 @@
 #  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  // SPDX-License-Identifier: BSD
 
-from typing import TypedDict, Union, Any, List
+from typing import TypedDict, Union, List, Any
 
 JOB_RESULTS_FILENAME = "job_results.json"
 RUN_FILENAME = "run.json"
@@ -9,6 +9,11 @@ RUN_FILENAME = "run.json"
 # URLs for EC2 metadata retrieval (IMDSv2)
 URL_IMDS_TOKEN = "http://169.254.169.254/latest/api/token"
 URL_IMDS_DOCUMENT = "http://169.254.169.254/latest/dynamic/instance-identity/document"
+
+
+class JobResults(TypedDict):
+    config: Any
+    metrics: Any
 
 
 class Versions(TypedDict):
@@ -25,10 +30,12 @@ class EC2Metadata(TypedDict):
     region: str
 
 
-class Run(TypedDict):
-    """Information about a Hydra run.
+class RunResults(TypedDict):
+    """Results from a Hydra run.
 
-    Also, a :class:`Run` object will be inserted as-is in DynamoDB."""
+    Note:
+        An instance of :class:`RunResults` will be inserted as-is in DynamoDB.
+    """
 
     run_id: str  # PK (Partition Key)
     timestamp_utc: float  # SK (Sort Key)
@@ -37,4 +44,4 @@ class Run(TypedDict):
     ec2_metadata: Union[EC2Metadata, None]
     run_elapsed_time_s: float
     number_of_jobs: int
-    all_job_results: List[Any]
+    job_results: List[JobResults]
---
+++ s3torchbenchmarking/utils/run_dataset_benchmarks.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#
+# Run dataset benchmarks.
+
+python ./src/s3torchbenchmarking/dataset/benchmark.py -cd conf -cn dataset
